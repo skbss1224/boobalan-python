ff49c5b55aac9e67bcd31f90076aecfe8f287bb5
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-08-11 10:53:48 +0530

    operator,strings
    python

---
+++ operators.py
@@ -0,0 +1,36 @@
+'''operators
+arithmetic operators
++ - plus operator
+- - minus operator
+% - percentage operator
+* - mul operator
+/ - div operator
+'''
+'''a=10
+b=10
+print(a+b)
+case senstive'''
+sasi=20000
+boobalan=45000
+totalsalary=sasi+boobalan
+print("total salary:",totalsalary)
+
+sasi=90000
+boobalan=45000
+totalsalary=sasi-boobalan
+print("minus operator:",totalsalary)
+
+sasi=90000
+boobalan=45000
+totalsalary=sasi*boobalan
+print("mul operator:",totalsalary)
+
+sasi=90000
+boobalan=45000
+totalsalary=sasi%boobalan
+print("per operator:",totalsalary)
+
+sasi=90000
+boobalan=45000
+totalsalary=sasi/boobalan
+print("div operator:",totalsalary)
---
+++ strings.py
@@ -0,0 +1,13 @@
+alphabets="abcdefghijklmnopqrstuvwxyz"
+print(alphabets.upper())
+hello="MANI"
+print(hello.lower())
+print(alphabets)
+print(alphabets[0:4])
+print(alphabets[0:10])
+print(alphabets[0:25:3])
+print(alphabets[-5])
+print(alphabets[-1:10])
+#raw string
+computer=R"mycomputer\localdisk\newfloder"
+print(computer)   
