0de1eaa5a137417b2925fef8f4bdef75c500b0a5
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-08-12 11:05:04 +0530

    runtime
    python

---
+++ runtime.py
@@ -0,0 +1,26 @@
+
+#datatype
+'''a=50
+print(type(a))
+b=45.89
+print(type(b))
+c='hello'
+print(type(c))
+d=True
+print(type(d))'''
+
+#runtime
+'''sasi=int(input("enter the sasi salary"))
+boobalan=int(input("enter the boobalan salary"))
+print("plus operator",sasi+boobalan)
+print("minus operator",sasi-boobalan)
+print("mul operator",sasi*boobalan)
+print("div operator",sasi/boobalan)
+print("per operator",sasi%boobalan)'''
+
+cent=float(input("enter the cent value"))
+acre=cent/100
+print("cent value is",cent,"acre value is",acre)
+
+
+
