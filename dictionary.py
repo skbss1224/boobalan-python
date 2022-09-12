33e29f17573e672a9131c952d096a5f77513ea1c
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-09-12 16:20:45 +0530

    dictionary
    python

---
+++ dictionary.py
@@ -0,0 +1,63 @@
+'''thisdict={"brand":"ford","electric":"false","year":"1964","colors":["red","white","blue"]}
+print(thisdict)
+print(type(thisdict))'''
+
+'''thisdict={"brand":"ford","model":"mustang","year":1964}
+x=thisdict["year"]
+print(x)'''
+
+#insert
+'''car={"brand":"ford","model":"mustang","year":1964}
+x=car.keys()
+print(x)
+car["color"]="white"
+print(x)'''
+
+#update
+'''car={"brand":"ford","model":"mustang","year":1964}
+x=car.items()
+print(x)
+car["year"]="2020"
+print(x)'''
+
+'''car={"brand":"ford","model":"mustang","year":1964}
+car.update({"year":"2020"})
+print(car)'''
+
+#remove
+'''car={"brand":"ford","model":"mustang","year":1964}
+car.pop("model")
+print(car)'''
+
+'''car={"brand":"ford","model":"mustang","year":1964}
+del car["model"]
+print(car)'''
+
+#for
+'''car={"brand":"ford","model":"mustang","year":1964}
+for x in car:
+    print(car[x])'''
+
+#set of family
+'''members={"child":{"name":"sasi","year":2021},
+          "child2":{"name":"boobalan","year":2020}}
+print(members)'''
+
+'''
+operators=arithmetic,logical,relationoperators
+loan agency
+control statement=if else elif,bus ticket reservation
+list,tuple,dic,set,
+for,while,patters,strings,
+
+function
+oops=>set of instructions
+exception handling
+multi threading
+t kinter
+(project)
+my sql
+core python
+front end
+html,css,bootstrap,js
+'''
