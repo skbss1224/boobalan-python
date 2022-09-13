7462300b8413e8e57954bd202c92c8865890975c
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-09-13 16:26:43 +0530

    function
    python

---
+++ functions.py
@@ -0,0 +1,27 @@
+
+'''def boopalan(name):
+    print(name+"refness") 
+boopalan("email")
+boopalan("boobalan")
+boopalan("sasi")'''
+
+'''
+def funct(child1,child2,child3):
+    print("youngest child is"," "+child1)
+    print("medium child is"," "+child2)
+    print("biggest child is"," "+child3)
+
+funct(child1="email",child2="boobalan",child3="sasi")'''
+
+'''def sasi(country="norway"):
+    print("iam from "+country)
+sasi("sweden")
+sasi()
+sasi("india")
+sasi("brazil")'''
+
+def func(food):
+    for x in food:
+        print(x)
+fruits=['apple','banana','cherry']
+func(fruits)
